a876940d9fff35714d5b74075829c5d926c766f7
Lucas Oliveira <olivmath@protonmail.com>
2024-01-25 01:22:52 -0300

    fix: clarify notation about brute pointer, remove deref maked by pattern match in signature of closure (|&leaf|) and add  deref in params to better readability

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,16 @@ pub unsafe extern "C" fn run1() {
 ///
 /// FFI to Python.
 #[no_mangle]
-pub unsafe extern "C" fn run2(leaves_ptr: *const *const u8, len_leaves: usize) {
+pub unsafe extern "C" fn run2(leaves_ptr: *mut *mut u8, len_leaves: usize) {
     let leaves = unsafe { slice::from_raw_parts(leaves_ptr, len_leaves) }
         .iter()
-        .map(|&leaf_ptr| unsafe { slice::from_raw_parts(leaf_ptr, 32).to_vec() })
+        .map(|leaf_ptr| unsafe { slice::from_raw_parts(*leaf_ptr, 32).to_vec() })
         .collect::<Vec<Vec<u8>>>();
 
     for leaf in leaves {
+
         println!("RUST SIDE: {leaf:?}");
     }
+
+    
 }
