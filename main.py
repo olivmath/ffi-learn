1e6e5f25b952cb6434ed2362317a4553cbe04546
Lucas Oliveira <olivmath@protonmail.com>
2024-01-25 00:56:33 -0300

    PROGRESS: now we can pass a List[bytes] to Rust by ffi

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import ctypes
+from hashlib import sha256
+from typing import List
 
 
 class Lib:
     def __init__(self) -> None:
         self.lib = ctypes.CDLL("./lib.dylib")
 
+        # RUN 1: Call function that just print something
         self.lib.run1.argtypes = []
         self.lib.run1.restype = None
+
+        # RUN 2: Call function that send a list[bytes] and print it
+        self.lib.run2.argtypes = [
+            ctypes.POINTER(ctypes.POINTER(ctypes.c_ubyte)),
+            ctypes.c_size_t,
+        ]
+        self.lib.run2.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_ubyte))
 
     def run1(self):
         print("PYTHON SIDE: Hello FFI")
         self.lib.run1()
 
+    def run2(self, leaves: List[bytes]):
+        for leaf in leaves:
+            print(f"PYTHON SIDE: {list(leaf)}")
+
+        len_leaves = len(leaves)
+        leaf_pointers = (ctypes.POINTER(ctypes.c_ubyte) * len_leaves)()
+
+        for i, leaf in enumerate(leaves):
+            array_type = ctypes.c_ubyte * 32
+            leaf_pointers[i] = array_type(*leaf)
+
+        self.lib.run2(leaf_pointers, len_leaves)
+
 
 lib = Lib()
 lib.run1()
+leaves_input = [sha256(data.encode()).digest() for data in ["a", "b", "c", "d"]]
+lib.run2(leaves_input)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
+use std::slice;
+
+/// # Safety
+///
+/// FFI to Python.
 #[no_mangle]
 pub unsafe extern "C" fn run1() {
     println!("RUST SIDE: Hello FFI");
+}
+
+/// # Safety
+///
+/// FFI to Python.
+#[no_mangle]
+pub unsafe extern "C" fn run2(leaves_ptr: *const *const u8, len_leaves: usize) {
+    let leaves = unsafe { slice::from_raw_parts(leaves_ptr, len_leaves) }
+        .iter()
+        .map(|&leaf_ptr| unsafe { slice::from_raw_parts(leaf_ptr, 32).to_vec() })
+        .collect::<Vec<Vec<u8>>>();
+
+    for leaf in leaves {
+        println!("RUST SIDE: {leaf:?}");
+    }
 }
