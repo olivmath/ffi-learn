f8c18694371729a2a285ebd851e4b359d4b4c5dc
Lucas Oliveira <olivmath@protonmail.com>
2024-01-26 23:50:27 -0300

    PROGRESS: now we can pass a List[bytes] and hash into Rust side by ffi

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import ctypes
+from keccaky import hash_it_bytes
 from hashlib import sha256
 from typing import List
+import ctypes
 
 
 class Lib:
     def __init__(self) -> None:
-        self.lib = ctypes.CDLL("./lib.dylib")
+        self.lib = ctypes.CDLL("./target/release/libffi_learn.dylib")
+        # self.lib = ctypes.CDLL("./lib.dylib")
 
         # RUN 1: Call function that just print something
         self.lib.run1.argtypes = []
@@ -17,6 +19,13 @@ class Lib:
             ctypes.c_size_t,
         ]
         self.lib.run2.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_ubyte))
+
+        # RUN 3: Call function that send a list[bytes] and hash leaves and print it
+        self.lib.run3.argtypes = [
+            ctypes.POINTER(ctypes.POINTER(ctypes.c_ubyte)),
+            ctypes.c_size_t,
+        ]
+        self.lib.run3.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_ubyte))
 
     def run1(self):
         print("PYTHON SIDE: Hello FFI")
@@ -35,8 +44,23 @@ class Lib:
 
         self.lib.run2(leaf_pointers, len_leaves)
 
+    def run3(self, leaves: List[bytes]):
+        for leaf in leaves:
+            leaf = hash_it_bytes(leaf)
+            print(f"PYTHON SIDE: {list(leaf)}")
+
+        len_leaves = len(leaves)
+        leaf_pointers = (ctypes.POINTER(ctypes.c_ubyte) * len_leaves)()
+
+        for i, leaf in enumerate(leaves):
+            array_type = ctypes.c_ubyte * 32
+            leaf_pointers[i] = array_type(*leaf)
+
+        self.lib.run3(leaf_pointers, len_leaves)
+
 
 lib = Lib()
 lib.run1()
 leaves_input = [sha256(data.encode()).digest() for data in ["a", "b", "c", "d"]]
 lib.run2(leaves_input)
+lib.run3(leaves_input)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
 use std::slice;
+use tiny_keccak::{Hasher, Keccak};
+
+pub fn hash_it(data: &[u8], result: &mut [u8; 32]) {
+    let mut k256 = Keccak::v256();
+
+    k256.update(data);
+    k256.finalize(result);
+}
 
 /// # Safety
 ///
@@ -12,16 +20,30 @@ pub unsafe extern "C" fn run1() {
 ///
 /// FFI to Python.
 #[no_mangle]
-pub unsafe extern "C" fn run2(leaves_ptr: *mut *mut u8, len_leaves: usize) {
+pub unsafe extern "C" fn run2(leaves_ptr: *const *const u8, len_leaves: usize) {
     let leaves = unsafe { slice::from_raw_parts(leaves_ptr, len_leaves) }
         .iter()
         .map(|leaf_ptr| unsafe { slice::from_raw_parts(*leaf_ptr, 32).to_vec() })
         .collect::<Vec<Vec<u8>>>();
 
     for leaf in leaves {
-
         println!("RUST SIDE: {leaf:?}");
     }
+}
 
-    
+/// # Safety
+///
+/// FFI to Python.
+#[no_mangle]
+pub unsafe extern "C" fn run3(leaves_ptr: *mut *mut u8, len_leaves: usize) {
+    let leaves: Vec<Vec<u8>> = unsafe { slice::from_raw_parts(leaves_ptr, len_leaves) }
+        .iter()
+        .map(|leaf_ptr| unsafe { slice::from_raw_parts(*leaf_ptr, 32).to_vec() })
+        .collect();
+
+    for leaf in leaves {
+        let mut result: [u8; 32] = [0; 32];
+        hash_it(&leaf, &mut result);
+        println!("RUST SIDE: {result:?}");
+    }
 }
